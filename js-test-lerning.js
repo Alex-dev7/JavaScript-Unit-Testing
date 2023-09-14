7d6e192be04536f4da696512d3a96233dbbb87b2
Alexei Rusu <rusualexei1995@gmail.com>
2023-09-13 20:16:17 -0400

    initial commit

---
+++ js-test-lerning.js
@@ -0,0 +1,17 @@
+// Regression
+
+// When adding a new feature to your product, it’s possible that something will break. If that break occurs within a feature developed earlier, it is called regression. When functionality previously developed and tested stops working, you may say the functionality regressed.
+
+// Running an automated test suite is fast and repeatable, which means you can run tests after every change to confirm that old features still work. If they have regressed, the test output should notify you.
+
+
+
+// Writing automated tests takes time, but the cost is outweighed by the benefits. Automated testing
+
+// Increases confidence that your product works as expected (compared to manual testing)
+// Improves upon documentation
+// Reduces the likelihood of regression
+// You also learned
+
+// Where and why test code is stored alongside implementation code
+// Terms to help communicate the benefit of testing: manual testing, automated testing, test suite, bug, documentation, and regression
