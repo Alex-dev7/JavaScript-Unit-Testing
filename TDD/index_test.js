eff32f40c56d468a9cacb8e7edcd6034ad0fab98
Alexei Rusu <rusualexei1995@gmail.com>
2023-09-18 19:47:09 -0400

    second test

--- TDD/index_test.js
+++ TDD/index_test.js
@@ -14,5 +14,18 @@ describe('Calculate', () => {
       // verification
       assert.strictEqual(result, expected);
     });
+
+    // Second test 
+    it('returns the sum ofa four-item list', () => {
+      // Setup 
+      const expectedResult = 22;
+      const inputArray = [4, 5, 6, 7]
+
+      // Exercise
+      const result = Calculate.sum(inputArray)
+
+      // Verification
+      assert.strictEqual(result, expectedResult);
+    })
   });
 });
