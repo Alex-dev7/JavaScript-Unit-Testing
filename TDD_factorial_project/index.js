5c36c30291ae9706b79105b4259ea609eb990291
Alexei Rusu <rusualexei1995@gmail.com>
2023-09-18 20:46:12 -0400

    factorial method tests

--- TDD_factorial_project/index.js
+++ TDD_factorial_project/index.js
@@ -1,5 +1,14 @@
 const Calculate = {
-  
-}
-
-module.exports = Calculate;
+    factorial(num){
+        if(num === 0) {return 1}
+        let result = num
+        
+        while((num - 1 ) !== 0){ 
+          result *= (num - 1 )
+          num--
+        }
+        return result
+      }
+    }
+    
+    module.exports = Calculate;
--- TDD_factorial_project/index_test.js
+++ TDD_factorial_project/index_test.js
@@ -1,8 +1,42 @@
-var assert = require("assert");
-var Calculate =  require('./index.js')
+const assert = require("assert");
+const Calculate =  require('../index.js')
 
 describe('Calculate', () => {
   describe('.factorial', () => {
+    it('will test if the output of 5! is equal to 120', () => {
+      // setup
+      const expectedResult = 120
+      const input = 5
 
+      // exercise
+      const result = Calculate.factorial(input)
+
+      // verification
+      assert.strictEqual(result, expectedResult)
+    })
+
+      it('will test if the output of 3! is equal to 6', () => {
+      // setup
+      const expectedResult = 6
+      const input = 3
+
+      // exercise
+      const result = Calculate.factorial(input)
+
+      // verification
+      assert.strictEqual(result, expectedResult)
+    })
+
+    it('returns 1 when you pass in 0', () => {
+      // setup 
+      const expectedResult = 1
+      const input = 0
+
+      // exercice
+      const result = Calculate.factorial(input)
+
+      // verification
+      assert.strictEqual(result, expectedResult)
+    })
   });
 });
